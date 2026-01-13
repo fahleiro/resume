6744e078e90ca5877d1e40900492388c4a1e9f9f
gabriel-faleiro <gabrielreinischii@gmail.com>
2026-01-13 08:51:07 -0300

    fix: database card

---
+++ js/cards/database.js
@@ -0,0 +1,38 @@
+export default {
+    id: 'database',
+    title: 'Banco de Dados',
+    summary: 'Utilização de bancos de dados relacionais e NoSQL em validação de dados, integrações e aplicações próprias',
+    content: `
+        <ul>
+
+        <li>
+            <p>
+                Utilização de PostgreSQL em aplicações e serviços containerizados,
+                incluindo modelagem básica, consultas SQL e integração com APIs.
+            </p>
+        </li>
+
+        <li>
+            <p>
+                Experiência com SQL Server utilizando SQL Server Management Studio (SSMS),
+                atuando em consultas, análise de dados, procedures e triggers. Incluindo validações de integração.
+            </p>
+        </li>
+
+        <li>
+            <p>
+                Utilização de Oracle Database via Oracle SQL Developer,
+                com foco em análise de dados e testes integrados.
+            </p>
+        </li>
+
+        <li>
+            <p>
+                Utilização de MongoDB como banco NoSQL de apoio à plataforma própria de DeviceFarm,
+                garantindo arquivamento de logs de sessões Appium, entre dispositivos e gerenciamento de roles.
+            </p>
+        </li>
+
+        </ul>
+    `
+};
